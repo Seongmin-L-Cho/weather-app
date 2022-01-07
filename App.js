f6d1d646e172a71b906a8878c6fbb767a4e1275f
DESKTOP-NJGRTB8\giner <Seongmin.L.Cho@gmail.com>
2022-01-07 20:23:26 +0900

    fix : temp commit

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import Loading from "./Loading";
-import { Alert } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
+import Constants from "expo-constants";
+import { Fontiso } from "@expo/vector-icons";
 import {
   View,
   Text,
@@ -10,20 +11,28 @@ import {
   StyleSheet,
   ScrollView,
 } from "react-native";
-import axios from "axios";
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const API_KEY = "75e30da1add10c5eb5de39cdf6e265fd";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Snow: "snow",
+  Rain: "rains",
+  Thunderstorm: "lightning",
+  Drizzle: "rain",
+  Atmosphere: "cloudy-gusts",
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
   const [ok, setOk] = useState();
   const [days, setDays] = useState([]);
-  getWeater = async (latitude, longitude) => {};
 
   const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
-    if (granted) {
+    if (!granted) {
       setOk(false);
     }
     const {
@@ -34,6 +43,7 @@ export default function App() {
       { latitude, longitude },
       { useGoogleMaps: false }
     );
+
     setCity(location[0].district);
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alert&units=metric&appid=${API_KEY}`
@@ -59,7 +69,7 @@ export default function App() {
         contentContainerStyle={styles.weather}
       >
         {days.length === 0 ? (
-          <View style={styles.day}>
+          <View style={{ ...styles.day, alignItems: "center" }}>
             <ActivityIndicator
               color="white"
               style={{ marginTop: 10 }}
@@ -69,9 +79,23 @@ export default function App() {
         ) : (
           days.map((day, index) => (
             <View key={index} style={styles.day}>
-              <Text style={styles.temp}>
-                {parseFloat(day.temp.day).toFixed(1)}
-              </Text>
+              <View
+                stlye={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={68}
+                  color="white"
+                />
+              </View>
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.tinyText}>{day.weather[0].description}</Text>
             </View>
